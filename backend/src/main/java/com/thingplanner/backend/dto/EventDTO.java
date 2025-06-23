37b95b53489e619a7178cf8b7ec8a1e8df022aa3
OliverKeefe <oliverkeefe@tutanota.com>
2025-06-23 20:50:26 +0100

    Add EventDTO

---
+++ backend/src/main/java/com/thingplanner/backend/dto/EventDTO.java
@@ -0,0 +1,31 @@
+package com.thingplanner.backend.dto;
+
+public class EventDTO {
+    private Long eventId;
+    private String eventName;
+    private int eventType;
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(int eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+}
