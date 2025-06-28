5ca66a7208d60714a3a40835d81ece1b59b3a0ea
OliverKeefe <oliverkeefe@tutanota.com>
2025-06-28 14:35:43 +0100

    Add create method

--- backend/src/main/java/com/thingplanner/backend/service/EventService.java
+++ backend/src/main/java/com/thingplanner/backend/service/EventService.java
@@ -4,7 +4,9 @@ import com.thingplanner.backend.dto.request.EventRequest;
 import com.thingplanner.backend.dto.response.EventResponse;
 import com.thingplanner.backend.entity.EventEntity;
 import com.thingplanner.backend.mapper.EventMapper;
+import com.thingplanner.backend.mapper.EventTypeMapper;
 import com.thingplanner.backend.repository.EventRepository;
+import org.apache.coyote.Response;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +23,20 @@ public class EventService {
     public EventService(EventRepository eventRepository, EventMapper eventMapper) {
         this.eventRepository = eventRepository;
         this.eventMapper = eventMapper;
+    }
+
+    public EventResponse create(EventRequest request) {
+        if (findByFields(request) == null) {
+            try {
+                EventEntity eventEntity = eventMapper.toEntity(request);
+                eventRepository.save(eventEntity);
+                return eventMapper.toResponse(eventEntity);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create event", e);
+            }
+        } else {
+            throw new RuntimeException("Event already exists.");
+        }
     }
 
     public List<EventResponse> findByFields(EventRequest request) {
