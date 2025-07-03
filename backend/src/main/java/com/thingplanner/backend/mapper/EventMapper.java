0de162689b4273da38b059c9af1d468f0c28b7e2
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-03 21:09:26 +0100

    Add toUpdatedEntity method

--- backend/src/main/java/com/thingplanner/backend/mapper/EventMapper.java
+++ backend/src/main/java/com/thingplanner/backend/mapper/EventMapper.java
@@ -19,6 +19,8 @@ public class EventMapper {
     public EventEntity toEntity(EventRequest request) {
         EventEntity entity = new EventEntity();
         EventTypeEntity eventType = eventTypeMapper.toEntity(request.getEventType());
+
+        if (request.getEventId() != null) entity.setId(request.getEventId());
         entity.setEventName(request.getEventName());
         //entity.setDescription(dto.getDescription());
         entity.setStartDate(request.getStartDate());
@@ -36,5 +38,21 @@ public class EventMapper {
         response.setEndDate(entity.getEndDate());
         response.setEventType(eventTypeMapper.toResponse(entity.getEventType()));
         return response;
+    }
+
+    public EventEntity toUpdatedEntity(EventEntity currentEntity, EventEntity updatedEntity) {
+        if (!updatedEntity.getEventName().equals(currentEntity.getEventName()) && updatedEntity.getEventName() != null) {
+            currentEntity.setEventName(updatedEntity.getEventName());
+        }
+        if (!updatedEntity.getStartDate().equals(currentEntity.getStartDate()) && updatedEntity.getEventName() != null) {
+            currentEntity.setStartDate(updatedEntity.getStartDate());
+        }
+        if (!updatedEntity.getEndDate().equals(currentEntity.getEndDate()) && updatedEntity.getEventName() != null) {
+            currentEntity.setEndDate(updatedEntity.getEndDate());
+        }
+        if (updatedEntity.getEventType() != currentEntity.getEventType() && updatedEntity.getEventName() != null) {
+            currentEntity.setEventType(updatedEntity.getEventType());
+        }
+        return currentEntity;
     }
 }
