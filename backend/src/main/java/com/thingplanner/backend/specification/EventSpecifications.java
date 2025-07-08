2a5bcaba5d59e42d1cb5ca03010a7616a8c0d033
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-08 16:14:01 +0100

    Refactor eventName to name

--- backend/src/main/java/com/thingplanner/backend/specification/EventSpecifications.java
+++ backend/src/main/java/com/thingplanner/backend/specification/EventSpecifications.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.domain.Specification;
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.criteria.Predicate;
+import org.yaml.snakeyaml.util.Tuple;
 
 
 public class EventSpecifications {
@@ -14,22 +15,18 @@ public class EventSpecifications {
         return (root, criteriaQuery, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
-            if (request.getEventName() != null && !request.getEventName().isBlank()) {
-                predicates.add(criteriaBuilder.equal(root.get("eventName"), request.getEventName()));
+            if (request.getName() != null && !request.getName().isBlank()) {
+                predicates.add(criteriaBuilder.equal(root.get("eventName"), request.getName()));
             }
-
             if (request.getEventType() != null && request.getEventType().getId() != null) {
                 predicates.add(criteriaBuilder.equal(root.get("eventType").get("id"), request.getEventType().getId()));
             }
-
             if (request.getStartDate() != null && !request.getStartDate().isBlank()) {
                 predicates.add(criteriaBuilder.equal(root.get("startDate"), request.getStartDate()));
             }
-
             if (request.getEndDate() != null && !request.getEndDate().isBlank()) {
                 predicates.add(criteriaBuilder.equal(root.get("endDate"), request.getEndDate()));
             }
-
             //if (request.getAttendeeEmails() != null && !request.getAttendeeEmails().isEmpty()) {
             //    Join<EventEntity, String> attendeesJoin = root.join("attendeeEmails");
             //    request.getAttendeeEmails().forEach(email ->
@@ -39,5 +36,21 @@ public class EventSpecifications {
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
+    }
+
+    //public static Specification<EventEntity> eventSearchSpecification(EventRequest request, EventFilters eventFilters) {
+    //    return (root, query, criteriaBuilder) -> {
+    //        List<Predicate> predicates = new ArrayList<>();
+//
+    //        if (request.getEventName() != null && !request.getEventName().isBlank()) {
+    //            predicates.add(criteriaBuilder.like(root.get("eventName"), request.getEventName()));
+    //        }
+    //        //if ()
+    //    };
+    //}
+
+    public static class EventFilters {
+        private Tuple<String, String> dateRange;
+        private Integer attendeeCount;
     }
 }
