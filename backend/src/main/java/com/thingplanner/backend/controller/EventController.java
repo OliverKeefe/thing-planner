588b3372702ad28d059d4343b7e93c6578a50732
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-10 13:41:40 +0100

    Remove @CrossOrigins annotation from controller

--- backend/src/main/java/com/thingplanner/backend/controller/EventController.java
+++ backend/src/main/java/com/thingplanner/backend/controller/EventController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/event")
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 public class EventController {
 
     private final EventService eventService;
