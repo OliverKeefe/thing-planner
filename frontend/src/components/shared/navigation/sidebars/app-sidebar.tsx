2f921d88751d222450977a2511e7b94689ca8318
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-17 20:56:51 +0100

    Add Sidebar group labels

--- frontend/src/components/shared/navigation/sidebars/app-sidebar.tsx
+++ frontend/src/components/shared/navigation/sidebars/app-sidebar.tsx
@@ -2,17 +2,28 @@ import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
-    SidebarGroup,
+    SidebarGroup, SidebarGroupLabel,
     SidebarHeader,
 } from "@/components/ui/sidebar"
+import CreateEventDialog from "@/features/planner/components/dialog/forms/CreateEventDialog.tsx";
+import AppTopbar from "@/components/shared/navigation/sidebars/app-topbar.tsx";
 
 export function AppSidebar() {
     return (
         <Sidebar>
-            <SidebarHeader />
+            <AppTopbar />
+            <SidebarHeader>
+            </SidebarHeader>
             <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+                <SidebarGroup >
+                    <SidebarContent>
+                        <SidebarGroupLabel>Application</SidebarGroupLabel>
+                        <CreateEventDialog />
+
+                        <SidebarGroupLabel>Events</SidebarGroupLabel>
+
+                    </SidebarContent>
+                </SidebarGroup>
             </SidebarContent>
             <SidebarFooter />
         </Sidebar>
